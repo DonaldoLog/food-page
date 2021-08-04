146807893540db9c4ca2230dd6295c8b8dbe2033
luis.lopez <luis.lopez@rappi.com>
2021-08-03 22:23:31 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      green: colors.green,
+      yellow: colors.yellow
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    extend: {},
+  },
+  plugins: [],
+}
